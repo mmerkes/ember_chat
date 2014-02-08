af9fcebc28cc7a2448b0b5144e85c63bfd3ec0f4
Empty Arsenal <matt.merkes@gmail.com>
2014-02-08 12:39:02 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Welcome the Ember Chat!
+
+Basically, I'm playing around with Ember.js and Socket.io to see
+if I can get them to play nicely together. The server is Node.js, 
+of course, and the extent of the functionality is still to be determined.
---
+++ app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//require('./routes')(app);
+require('./lib/chat_server.js')(server);
+
+app.get('/', function( request, response ) {
+	response.sendfile(__dirname + '/public/index.html');
+});
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
